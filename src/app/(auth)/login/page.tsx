99055c9061f28c6912d097c7235d85091e153f8b
Adhitama Fikri Kusumahendra W <adhitamafikri.developer@gmail.com>
2025-08-10 09:56:44 +0700

    chore: prepare shadcn/ui integration, generate button ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function LoginPage() {
-  return <h1>Login Page</h1>;
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <Button className='bg-primary-500 text-white px-4 py-2 rounded-md'>Login Please</Button>
+    </div>
+  );
 }
 
 export default LoginPage;
