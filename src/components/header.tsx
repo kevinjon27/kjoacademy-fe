97a3dd6ec5f7b34f0103832e51db07049794297e
Adhitama Fikri Kusumahendra W <adhitamafikri.developer@gmail.com>
2025-08-16 09:12:50 +0700

    chore: border bottom for the main header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export async function Header() {
   const currentLocale = cookieStore.get("locale")?.value || "id";
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b border-primary-700 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
